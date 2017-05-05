3b5c2ed469c7362c54fabc0878ebdf1ae92d4265
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-05-05 19:03:31 +0200

    Initial Commit: Contains pause menu and scene switching
    Known bugs: Opening pause menu does not prevent the player from clicking
    buttons on lower layers.

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+	//----------------------------------------
+	// handles
+	public UIHandler UH;
+
+	//-----------------------------------------
+	// function definitions
+	public void chapter(string scene) {
+		SceneManager.LoadScene (scene);
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("Test");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+	public GameHandler GH;
+	public SoundHandler SH;
+
+	private bool mute;
+
+	private float volume;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+		// Update is called once per frame
+	void Update () {
+		ScanForKeyStroke();
+	}
+
+	void ScanForKeyStroke() {
+		if (Input.GetKeyDown("escape")) TogglePauseMenu();
+	}
+
+	public void TogglePauseMenu()
+	{
+		// not the optimal way but for the sake of readability
+		if (GetComponentInChildren<Canvas>().enabled)
+		{
+			GetComponentInChildren<Canvas>().enabled = false;
+			Time.timeScale = 1.0f;
+		}
+		else
+		{
+			GetComponentInChildren<Canvas>().enabled = true;
+			Time.timeScale = 0f;
+		}
+
+		Debug.Log("GAMEMANAGER:: TimeScale: " + Time.timeScale);
+	}
+
+	//-----------------------------------------------------------
+	// Music Settings Function Definitions
+	public void MusicSliderUpdate(float val) {
+		volume = val;
+		if (!mute) {
+			SH.SetVolume(val);
+		}
+	}
+
+	public void ToggleSound() {
+		if (mute) {
+			mute = false;
+			SH.SetVolume (volume);
+		} else {
+			mute = true;
+			SH.SetVolume (0f);
+		}
+	}
+}
