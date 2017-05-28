aa32e8eb426dedbf3132aca2fc593c4c8bb31619
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-05-28 15:54:02 +0200

    Changed some icons/graphics and started helper figure
    KNOWN ISSUES: The speech bubble resizes on changes in text, but only in
    the editor, not at runtime.

---
+++ Assets/Scripts/UI/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Helper : MonoBehaviour {
+
+	public GameObject speechBubble;
+	public GameObject textBox;
+
+	void OnMouseDown () {
+		textBox.GetComponent<Text> ().text = "Something that I would want to write in here to test the sizing of the text. Apparently I can change the text, but the fitter does not work.";
+	}
+}
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -14,6 +14,7 @@ public class UIHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
+		pause_menu.SetActive (false);
 	}
 
 	// Update is called once per frame
