b65acfb3b20af160f1b247c6241ca2c130027030
fetzold <felix.etzold@live.de>
2017-06-09 07:03:54 +0200

    Navigator gebaut, noch Fehlerhaft

---
+++ Assets/Scripts/Interacables/ActivateHoverEffectNavigator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ActivateHoverEffectNavigator : MonoBehaviour,  IPointerEnterHandler, IPointerExitHandler {
+
+    public Image image;
+    private String name;
+
+    public void Start()
+    {
+        image.enabled = true;
+        name = image.name;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GameObject go = GameObject.Find(name + " Hover");
+        go.GetComponent<Image>().enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GameObject go = GameObject.Find(name + " Hover");
+        go.GetComponent<Image>().enabled = false;
+    }
+}
