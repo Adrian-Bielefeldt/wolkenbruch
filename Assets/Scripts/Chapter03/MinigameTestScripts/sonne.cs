295a72c631914e0e4e566ca330e104ffd04f1de9
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-07-04 13:51:45 +0200

    Added Chapter 01 and Chapter03

---
+++ Assets/Scripts/Chapter03/MinigameTestScripts/sonne.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonne : MonoBehaviour {
+    public GameObject strahl1;
+    public GameObject strahl2;
+    public GameObject strahl3;
+    public GameObject strahl4;
+    public GameObject strahl5;
+    public GameObject strahl6;
+    public GameObject strahl7;
+    public GameObject strahl8;
+    public GameObject strahl9;
+    public GameObject strahl10;
+    public GameObject strahl11;
+    public GameObject strahl12;
+    public GameObject strahl13;
+
+    // Use this for initialization
+    void Start()
+    {
+        strahl1.SetActive(false);
+        strahl2.SetActive(false);
+        strahl3.SetActive(false);
+        strahl4.SetActive(false);
+        strahl5.SetActive(false);
+        strahl6.SetActive(false);
+        strahl7.SetActive(false);
+        strahl8.SetActive(false);
+        strahl9.SetActive(false);
+        strahl10.SetActive(false);
+        strahl11.SetActive(false);
+        strahl12.SetActive(false);
+        strahl13.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos);
+            if (transform.rotation.z >= 0)
+            {
+                if (transform.rotation.z >= 27)
+                {
+                    strahl1.SetActive(true);
+                    strahl2.SetActive(true);
+                    return;
+                }
+                strahl1.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Chapter03/prepareMinigameChapter03.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class prepareMinigameChapter03 : MonoBehaviour, INotifiableHandler
+{
+    public UIHandler UI;
+    public GameObject informationScene;
+    public GameObject minigameScene;
+    public GameObject startMinigameButton;
+    public GameObject message;
+    public GameObject helpBubble;
+    public GameObject helper;
+
+    private bool[] checkIfAllInformationObjectAlreadyRead = new bool[6];
+
+    void Start()
+    {
+        minigameScene.SetActive(false);
+
+        Text text = message.GetComponent<Text>();
+        helpBubble.SetActive(true);
+        StartCoroutine(LastCall(2));
+        helper.transform.localScale = new Vector2(3, 3);
+        helper.transform.localPosition = new Vector2(0, -200);
+        helpBubble.transform.localPosition = new Vector2(-150, -100);
+        text.text = "Lorem Ipsum";
+        StopCoroutine("LastCall");
+
+        Interactable_Information[] list = informationScene.GetComponentsInChildren<Interactable_Information>();
+        foreach (Interactable_Information interactable in list)
+        {
+            interactable.handler = this;
+        }
+    }
+
+    public void notifyChange()
+    {
+        Interactable_Information[] list = informationScene.GetComponentsInChildren<Interactable_Information>();
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (list[i].alreadyRead)
+            {
+                checkIfAllInformationObjectAlreadyRead[i] = true;
+                for (int j = 0; j < checkIfAllInformationObjectAlreadyRead.Length; j++)
+                {
+                    if (checkIfAllInformationObjectAlreadyRead[j] == true && checkIfAllInformationObjectAlreadyRead[j + 1] == true && checkIfAllInformationObjectAlreadyRead[j + 2] == true && checkIfAllInformationObjectAlreadyRead[j + 3] == true && checkIfAllInformationObjectAlreadyRead[j + 4] == true && checkIfAllInformationObjectAlreadyRead[j + 5] == true)
+                    {
+                        startMinigameButton.SetActive(true);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void startMinigame()
+    {
+        informationScene.SetActive(false);
+        startMinigameButton.SetActive(false);
+        minigameScene.SetActive(true);
+    }
+
+    IEnumerator LastCall(int time)
+    {
+        yield return new WaitForSeconds(time);
+        helper.transform.localPosition = new Vector2(268, 0);
+        helpBubble.transform.localPosition = new Vector2(0, 0);
+        helper.transform.localScale = new Vector2(1, 1);
+        helpBubble.SetActive(false);
+    }
+}
